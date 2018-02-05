480feba032a02f8faef4604c4db32651984fd9c1
安藤涼平 <andouryouhei@andouryouhei-no-MacBook-Air.local>
2018-02-05 20:52:17 +0900

    create_user_model

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_aciton :authenticate_user!
+  before_aciton :configure_permitted_parameters, if :devise_controller?
+end
+
+protected
+
+def configure_permitted_parameters
+	devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
 end
---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,10 @@
+class UserController < ApplicationController
+
+	def edit
+		
+	end
+	def update
+		
+	end
+
+end
