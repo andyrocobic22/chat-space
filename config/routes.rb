22599bc33e5a65ff3337fa1118a95537e6342b46
安藤涼平 <andouryouhei@andouryouhei-no-MacBook-Air.local>
2018-02-09 12:50:52 +0900

    message→messages(controller)

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'groups#index'
   resources :groups, only: [:index, :new, :create, :edit, :update] do
-    resources :messages, only: [:index, :create]
+    resources :messages, only: [:index, :creta]
   end
   resources :users, only:[:edit, :update]
 end
