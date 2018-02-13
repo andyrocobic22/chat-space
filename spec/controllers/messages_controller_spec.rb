424217683547c364bddeb5a053036d624888e5e1
安藤涼平 <andouryouhei@andouryouhei-no-MacBook-Air.local>
2018-02-13 21:53:19 +0900

    test!

---
+++ spec/controllers/messages_controller_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+describe MessagesController do
+  describe 'GET #index' do
+    context "log_in" do
+      
+    end
+
+    context "log_out" do
+
+    end
+
+  end    
+end
